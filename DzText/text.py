84fd78607e178b9633edff84a889be4a390b545b
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-06-08 16:35:44 +0700

    Update text.py

--- DzText/text.py
+++ DzText/text.py
@@ -3,6 +3,12 @@
 #REEDIT BOLEH HAPUS CREDIT ANAK YATIM🤙
 
 #BASIC TEXT
-noadm = "Lu bukan admin tolol😭"
+no_adm = "Lu bukan admin tolol😭"
 repp = "Reply ke poto ya kontol"
 pross = "Processing..."
+usernf = "Kaga ketemu orangnya"
+
+#MODUL
+ban_1 = "Ngapain ban diri sendiri tolol🗿"
+ban_2 = "Developer gw mau lu ban? emng gw bot apaan💃"
+ban_3 = "Mana bisa ban admin group🤣"
