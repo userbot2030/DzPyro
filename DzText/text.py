e6808ee3d1a4071e4a64603b94b5d3042d4cbfce
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-06-08 16:17:55 +0700

    Create text.py

---
+++ DzText/text.py
@@ -0,0 +1,8 @@
+@MSDZULQRNN
+#t.me/MSPR0JECT | t.me/envSample
+#REEDIT BOLEH HAPUS CREDIT ANAK YATIM🤙
+
+#BASIC TEXT
+noadm = "Lu bukan admin tolol😭"
+repp = "Reply ke poto ya kontol"
+pross = "Processing..."
