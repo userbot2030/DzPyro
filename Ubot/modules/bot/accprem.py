23ef0b7b6e6949e9c9e650a33238c467ac5d81f7
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-06-09 07:42:34 +0700

    Update accprem.py

--- Ubot/modules/bot/accprem.py
+++ Ubot/modules/bot/accprem.py
@@ -6,7 +6,7 @@ FSUB = "accdzuserbot"
 
 
 
-async def fsub(filter, client, update):
+async def joined(filter, client, update):
     if not FSUB:  
      return True
     user_id = update.from_user.id
@@ -16,20 +16,9 @@ async def fsub(filter, client, update):
         member = await client.get_chat_member(chat_id = FSUB, user_id = user_id)
     except UserNotParticipant:
         return False
-            try:
-                await msg.reply(
-                    f"TEXT",
-                    disable_web_page_preview=True,
-                    reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("ADMIN", url="https://t.me/MSDZULQRNN")],
-                        [InlineKeyboardButton("SUPPORT", url="https://t.me/MSPR0JECT"),
-                         InlineKeyboardButton("SUPPORT", url="https://t.me/dzlog")],
-                    ])
-                )
-                await msg.stop_propagation()
-            except ChatWriteForbidden:
-                pass
-    except ChatAdminRequired:
-        print(f"Kamu bukan admin!")
+    if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
+        return False
+    else:
+        return True
 
-accdz = filter.create(fsub)
+accdz = filter.create(joined)
