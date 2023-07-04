a152bd18a19496d009e5ecca224c9027d46c9d83
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-07-04 14:26:34 +0700

    Update limit.py

--- Ubot/modules/basic/limit.py
+++ Ubot/modules/basic/limit.py
@@ -26,7 +26,7 @@ async def spamban(client: Client, m: Message):
     mm = await m.reply_text("`Processing...`")
     await asyncio.sleep(1)
     await mm.delete()
-    spambot_msg = response.updates[1].message.id + 1
+    status = await client.get_messages("SpamBot", response.updates[1].message.id + 1)
     status = await client.get_messages(chat_id="SpamBot", message_ids=spambot_msg)
     await m.edit_text(f"~ {status.text}")
 
