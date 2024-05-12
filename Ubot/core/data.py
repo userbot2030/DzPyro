aac43a50c9d8c5f9faf80194c2fef0bdf314788a
Wildan2024 <158287720+Wildan2024@users.noreply.github.com>
2024-05-12 20:56:19 +0700

    data.py

--- Ubot/core/data.py
+++ Ubot/core/data.py
@@ -3,6 +3,6 @@ from Ubot import cmds
 class Data:
 
     text_help_menu = (
-        f"𝘿𝙕-𝙐𝙎𝙀𝙍𝘽𝙊𝙏🤖\n━━━━━━━━━━━━━━━━━━\nᴛʏᴘᴇ : ᴘʀᴇᴍɪᴜᴍ💎\nᴀᴄᴛɪᴠᴇ : 𝟷 ʙᴜʟᴀɴ\nᴏᴡɴᴇʀ : @MSDQQQ\nᴜᴘᴅᴀᴛᴇs : @DezetStore\n━━━━━━━━━━━━━━━━━━\nᴋʟɪᴋ ʙᴜᴛᴛᴏɴ ᴅɪʙᴀᴡᴀʜ ᴜɴᴛᴜᴋ ᴍᴇʟɪʜᴀᴛ ᴍᴏᴅᴜʟ !"
+        f"𝘿𝘼𝙉-𝙐𝙎𝙀𝙍𝘽𝙊𝙏🤖\n━━━━━━━━━━━━━━━━━━\nᴛʏᴘᴇ : ᴘʀᴇᴍɪᴜᴍ💎\nᴀᴄᴛɪᴠᴇ : 𝟷 ʙᴜʟᴀɴ\nᴏᴡɴᴇʀ : @mhmdwldnnnn\nᴜᴘᴅᴀᴛᴇs : @Disney_storeDan\n━━━━━━━━━━━━━━━━━━\nᴋʟɪᴋ ʙᴜᴛᴛᴏɴ ᴅɪʙᴀᴡᴀʜ ᴜɴᴛᴜᴋ ᴍᴇʟɪʜᴀᴛ ᴍᴏᴅᴜʟ !"
     )
     reopen = [[InlineKeyboardButton("Open", callback_data="reopen")]]
